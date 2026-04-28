30d5cc43d37b240818485625a9c055ff35df6c6f
juliamlhbn <juliamlhbn@outlook.com>
2026-04-28 10:51:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$baseUrl = 'https://187.228.205.92.host.secureserver.net/';
+$baseUrl = 'https://187.228.205.92.host.secureserver.net/?';
 
 function generateSegment($length) {
   $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
